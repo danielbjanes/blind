887794297f531c17330641e4fb471026d321107a
danielbjanes <git@danielbjanes.com>
2026-02-22 17:13:29 -0500

    add nix stuff

---
+++ flake.nix
@@ -0,0 +1,71 @@
+{
+  #https://github.com/the-nix-way/dev-templates/blob/main/elixir/flake.nix
+  description = "A Nix-flake-based Go development environment";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1"; # unstable Nixpkgs
+
+  outputs =
+    { self, ... }@inputs:
+
+    let
+      goVersion = 25; # Change this to update the whole stack
+
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+              overlays = [ inputs.self.overlays.default ];
+            };
+          }
+        );
+    in
+    {
+      overlays.default = final: prev: {
+        go = final."go_1_${toString goVersion}";
+      };
+
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShellNoCC {
+            packages = with pkgs; [
+              # go (version is specified by overlay)
+              go
+              raylib
+              # goimports, godoc, etc.
+              gotools
+
+              # X11
+              libX11
+              libXcursor
+              libXrandr
+              libXi
+              libXinerama
+              libXxf86vm
+
+              # Wayland
+              wayland
+              libxkbcommon
+
+              # GL
+              mesa
+
+              gcc
+
+              # https://github.com/golangci/golangci-lint
+              golangci-lint
+            ];
+          };
+        }
+      );
+    };
+}
